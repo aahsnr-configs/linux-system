9d8e979f63ce5b068ee133fa273beab87da93bf3
aahsnr-configs <ahsanur041@proton.me>
2026-02-19 13:37:35 +0600

    updated

--- tumbleweed/brew-pkgs.sh
+++ tumbleweed/brew-pkgs.sh
@@ -1,5 +1,3 @@
-brew install atuin bat btop cava enchant aspell fd git gh lazygit ripgrep tealdeer tmux yazi starship dust direnv fastfetch fzf pandoc zoxide git-lfs
-
 brew install --cask font-jetbrains-mono
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-noto-color-emoji
---
+++ tumbleweed/post-install/misc.sh
@@ -0,0 +1,2 @@
+# add the following line to /usr/share/applications/Mailspring.desktop Exec lin
+Exec=mailspring --password-store="gnome-libsecret" %U
