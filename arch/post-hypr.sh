cf489a3af44974df07fa37813fc19ad5feadf96d
aahsnr-configs <ahsanur041@proton.me>
2026-02-23 19:13:45 +0600

    updated

--- arch/post-hypr.sh
+++ arch/post-hypr.sh
@@ -1,8 +1,7 @@
-hyprpm update
-hyprpm add https://github.com/hyprwm/hyprland-plugins
-hyprpm add https://github.com/outfoxxed/hy3
-hyprpm add https://github.com/cpiber/hyprscroller
-hyprpm add https://github.com/raybbian/hyprtasking
+hyprpm remove https://github.com/hyprwm/hyprland-plugins
+hyprpm remove https://github.com/outfoxxed/hy3
+hyprpm remove https://github.com/cpiber/hyprscroller
+hyprpm remove https://github.com/raybbian/hyprtasking
 # hyprpm enable hy3
 # hyprpm enable hyprscroller
 # hyprpm enable hyprtasking
